434596b232037904b693e08e9a381723818200ff
JungJay <keunhee0920@gmail.com>
2024-10-08 17:10:52 +0900

    [24.10.08] employee 전체조회 기능 추가 완료

--- src/main/java/com/naver/project1/view/PrintResultname.java
+++ src/main/java/com/naver/project1/view/PrintResultname.java
@@ -1,4 +1,25 @@
 package com.naver.project1.view;
 
+import com.naver.project1.model.dto.EmployeeDTO;
+
+import java.util.List;
+
 public class PrintResultname {
+
+    public void printListEmployee(List<EmployeeDTO> selectAllInformation) {
+
+        for (EmployeeDTO employeeList : selectAllInformation){
+            System.out.println(employeeList);
+        }
+
+    }
+
+    public void printError(String errorMessage) {
+
+        switch (errorMessage){
+            case "selectError":
+                System.out.println("학생 조회가 실패되었습니다...");
+                break;
+        }
+    }
 }
